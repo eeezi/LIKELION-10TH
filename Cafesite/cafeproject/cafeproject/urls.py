09ecd01be5e24fb43f7e5e1ff97a9a265381c85c
eeezi <alloy0035@hanmail.net>
2022-05-22 17:29:02 +0900

    5회차 과제 - 카페 소개 사이트

---
+++ Cafesite/cafeproject/cafeproject/urls.py
@@ -0,0 +1,11 @@
+import imp
+from pkgutil import ImpImporter
+from django.contrib import admin
+from django.urls import path
+from cafeapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('detail/', views.detail, name='detail'),
+]
